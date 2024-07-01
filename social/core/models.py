da0c1406c121e0b080a0516dc6d39c03ab094922
One-percent-athlete <one.percent.athlete@gmail.com>
2024-07-01 12:35:38 +0900

    Add post model

--- social/core/models.py
+++ social/core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+import uuid 
+from datetime import datetime
 
 User = get_user_model()
 
@@ -14,3 +16,14 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user_id} - {self.user.username}"
     
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)     
+    user =  models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images', blank=True, null=True)
+    caption = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=datetime.now)
+    num_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
